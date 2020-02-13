344ed42a63026c048eda65452f8223612e42c0c0
ip11 <iprash011@gmail.com>
2020-02-13 07:49:37 +0530

    hi

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Prash' });
+  res.render('index', { title: 'PrashANTH' });
 });
 
 module.exports = router;
