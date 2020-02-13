477b023fcc3a4e6b4f26ea5d3e1b70cde4f14267
ip11 <iprash011@gmail.com>
2020-02-13 08:00:49 +0530

    ip11

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'PrashANTH' });
+  res.render('index', { title: 'PrashanthTeja' });
 });
 
 module.exports = router;
